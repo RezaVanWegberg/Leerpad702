498db9c4bcfa35fb2085007cf17f0d5421e32493
Reza <99068159@mydavinci.nl>
2022-03-07 11:34:20 +0100

    heb lightswitch gemaakt

--- lightswitch/script.js
+++ lightswitch/script.js
@@ -1,9 +1,27 @@
 var btn = document.createElement('button');
 btn.style.margin = '10px';
-btn.innerHTML = '...';
+btn.innerHTML = 'click here';
 document.body.appendChild(btn);
 
 // schijf hier tussen je code
+
+
+
+function buttonFunction(event){
+    if(document.body.style.backgroundColor == "black"){
+        document.body.style.backgroundColor = "yellow"
+        btn.innerHTML = "Switch off"
+        console.log("Switch on")
+    } else {
+        document.body.style.backgroundColor = "black"
+        btn.innerHTML = "Switch on"
+        console.log("Switch off")
+
+    }
+
+}
+
+btn.onclick = buttonFunction;
 
 
 
